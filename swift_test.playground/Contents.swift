34efc76e869209f3811f82836fa7fab4ee5193f7
guvanch-amangeldiyev-idf <guvanch.amangeldiyev@idfinance.com>
2022-05-17 02:32:20 +0300

    cons vs var

--- swift_test.playground/Contents.swift
+++ swift_test.playground/Contents.swift
@@ -2,15 +2,14 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-let y = 10
-
-var x = y + 10
-
-//for i in 1..<10{
-//    print("i equals \(i)")
-//}
 
 let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
 label.backgroundColor = UIColor.red
 
 label.text = greeting
+
+//:varibles
+let cons = "Hello World" //cons
+var str = "hello XCode" //varible
+
+str = "Changed"
---
+++ swift_test.playground/Resources/constans_varibles/const_var.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+
+let cons = "Hello World" //cons
+var str = "hello XCode" //varible
+
+str = "Changed"
