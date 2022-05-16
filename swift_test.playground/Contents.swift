bc2d680e1f6f2d21d1b98b3dc4a1134c7640c15b
guvanch-amangeldiyev-idf <guvanch.amangeldiyev@idfinance.com>
2022-05-17 02:11:37 +0300

    start swift basics

---
+++ swift_test.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let y = 10
+
+var x = y + 10
+
+//for i in 1..<10{
+//    print("i equals \(i)")
+//}
+
+let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+label.backgroundColor = UIColor.red
+
+label.text = greeting
