3d05a57cfe93a192f0cfc01524fb3fe33ed2ec2d
guvanch-amangeldiyev-idf <guvanch.amangeldiyev@idfinance.com>
2022-05-19 00:00:28 +0300

    fix

--- swift_test.playground/Contents.swift
+++ swift_test.playground/Contents.swift
@@ -120,6 +120,85 @@ nameAges = [:] //it is like  removeAll
 
 
 
+//sets
+
+let setOne = Set<String>()
+let setTwo: Set<String> = []
+var setThree: Set = [1,2,3,4]
+
+setThree.insert(6)
+setThree.insert(7) //true
+
+setThree.remove(6)
+
+let boolValue = setThree.contains(1)
+boolValue
+
+let setFromOnetothree: Set = [1,2,3,4]
+let setFromFourToNine: Set = [1,4,5,6,7,8,9]
+
+
+let allValueArray = setFromOnetothree.union(setFromFourToNine).sorted()
+let interection = setFromOnetothree.intersection(setFromFourToNine) //пересечение
+
+let notRepetedSet = setFromOnetothree.symmetricDifference(setFromFourToNine).sorted()
+let substractedValue = setFromOnetothree.subtracting(setFromFourToNine).sorted() // no repeting in setFromFourToNine
+
+
+//if else
+
+//let b = 3
+//
+//if 1...4 ~= b {
+//    print("hello")
+//}
+
+
+//instruction guard
+
+func someFunc(a: Int, b: Int){
+    guard a == b else{ return }
+    guard b == 11 else{ return }
+    
+    if a == b {
+        if b == 11 {
+            //code
+        }
+    }
+}
+
+for i in 1...5 {
+    guard i != 3 else { break }
+    print (i)
+}
+
+for i in 1...5 {
+    if i != 3{
+        break
+        
+    }
+    print (i)
+}
+
+let totalScore = 10
+
+switch totalScore{
+case 20:
+    print("yak")
+case 30, 35:
+    print("yak")
+case 40:
+    print("yak")
+case 60...100:
+    print("yak")
+default:
+    break
+}
+
+
+
+
+
 
 
 
